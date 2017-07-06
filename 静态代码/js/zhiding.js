ab7624c6dafe6f85d35f1c25fa1e54897925e51c
bill.mark <2286449127@qq.com>
2017-07-06 16:07:16 +0800

    样式调整

--- 静态代码/js/zhiding.js
+++ 静态代码/js/zhiding.js
@@ -9,20 +9,29 @@ $(function(){
 		let  topHeight = 115;   // 设置置顶模块以上 盒子的高度（如果是要超过A那么加上A的高度）
 
 		if (offSet >= topHeight) {  // 如果偏移量大于等于 顶部的高度
-
 			$("#fix").css({position: "fixed", top: 0,width:'100%'}); //盒子固定定位在顶部（top后面的值自己确定要多少）
 			$(".list-head-show").show();
 			//$('#fix').style.width=width2+'px';
 		} else {
-
 			$("#fix").css({position: "static"});  //如果偏移量少于顶部高度，则清除定位
 			$(".list-head-show").hide();
 		}
-
+		
 	});
 
+	// $(".list-icons-box").click(function(){
+	// 	if ($(this).parents("li").next().is(":hidden")) {
+	// 		$(this).find("span").removeClass("toggle-open").addClass("toggle-close");
+	// 		$(this).parents("li").next().show();           
+	// 	}else{
+	// 		$(this).find("span").removeClass("toggle-close").addClass("toggle-open");
+	// 		$(this).parents("li").next().hide();
+	// 	}
+	// })
+});
 
-	$(".list-icons-box").click(function(){
+//不采用注释里的代码，因为JQ不能获取VUE创建的动态节点
+$(".list-icons-box").on("click",function(){
 		if ($(this).parents("li").next().is(":hidden")) {
 			$(this).find("span").removeClass("toggle-open").addClass("toggle-close");
 			$(this).parents("li").next().show();           
@@ -30,6 +39,4 @@ $(function(){
 			$(this).find("span").removeClass("toggle-close").addClass("toggle-open");
 			$(this).parents("li").next().hide();
 		}
-	});
-});	
-
+})
